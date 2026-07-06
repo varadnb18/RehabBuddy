66f4508cb5e30c2131de399650a3e91e6304bd10
VaradNashikkar <136262207+varadnb18@users.noreply.github.com>
2026-07-07 01:11:25 +0530

    firebase api key is in .env

--- client/src/Components/FireBase/FireBase.jsx
+++ client/src/Components/FireBase/FireBase.jsx
@@ -10,13 +10,13 @@ import {
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAq7hUJUm6Kt_46IuhWrBmE6LU0MHrQp1U",
-  authDomain: "login-auth-b2242.firebaseapp.com",
-  databaseURL: "https://login-auth-b2242-default-rtdb.firebaseio.com", // ✅ Add Realtime Database URL
-  projectId: "login-auth-b2242",
-  storageBucket: "login-auth-b2242.appspot.com",
-  messagingSenderId: "410983669687",
-  appId: "1:410983669687:web:6f1ddfc6730a4f740e1232",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
